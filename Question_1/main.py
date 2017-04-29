1b8cf8e1195de652c43fbef98f905e4f240493ac
bfaure <bfaure23@gmail.com>
2017-04-29 17:57:20 -0400

    value iteration algorithm now works

--- Question_1/main.py
+++ Question_1/main.py
@@ -1,8 +1,71 @@
 import sys,os,time
+from copy import copy,deepcopy
 
+discount=0.99
+
+reward_dict={"s1":0.0,"s2":0.0,"s3":1.0,"s4":0.0}
+
+trans_dict= {"s1":{ "a1":[["s1",0.2],["s2",0.8]],"a2":[["s1",0.2],["s4",0.8]]},
+			 "s2":{ "a2":[["s2",0.2],["s3",0.8]],"a3":[["s2",0.2],["s1",0.8]]},
+			 "s3":{ "a4":[["s2",1.0]],           "a3":[["s4",1.0]]},
+			 "s4":{ "a1":[["s4",0.1],["s3",0.9]],"a4":[["s4",0.2],["s1",0.8]]}}
+
+def write_utilities(util_dict,device=sys.stdout):
+	device.write("Utilties:\n")
+	for key,value in util_dict.items():
+		device.write("%s:\t%0.5f\n"%(key,value))
+
+def U(s,prior_utils):
+	best_action_util=None 
+	for action,outcomes in trans_dict[s].items():
+		action_util=0.0
+		for s1,s1_prob in outcomes:
+			action_util+=(s1_prob*prior_utils[s1])
+		if best_action_util==None or action_util>best_action_util:
+			best_action_util=action_util
+			best_action=action
+	return reward_dict[s]+(discount*best_action_util),best_action
+
+# err = maximum error allowed in the utility of any state
+def value_iteration(err=0.001):
+	util_log=open("value_iteration-utils.tsv","w")
+	action_log=open("value_iteration-actions.tsv","w")
+	meta_log=open("value_iteration-meta.txt","w")
+
+	i=0
+	u0=copy(reward_dict) # create initial utilities distribution
+	for s,u in u0.items():
+		util_log.write("%s%s"%(s,"\t" if i!=len(u0)-1 else "\n"))
+		action_log.write("%s%s"%(s,"\t" if i!=len(u0)-1 else "\n"))
+		u0[s]=0.0
+		i+=1
+
+	d=0.0
+	i=0
+	while True:
+		i+=1
+		sys.stdout.write("\rOptimizing states %d | d: %0.8f"%(i,d))
+		sys.stdout.flush()
+
+		d=0.0
+		u1=copy(u0)
+
+		j=0
+		for s in reward_dict.keys():
+			u1[s],action = U(s,u0)
+			util_log.write("%0.10f%s"%(u1[s],"\t" if j!=len(reward_dict)-1 else "\n"))
+			action_log.write("%s%s"%(action,"\t" if j!=len(reward_dict)-1 else "\n"))
+			if abs(u1[s]-u0[s])>d: d=abs(u1[s]-u0[s])
+			j+=1
+
+		u0=copy(u1)
+		if d<(err*((1-discount)/discount)): break
+	sys.stdout.write("\n")
+	return u0
 
 def main():
-	print("hello world")
+	value_iteration()
+
 
 if __name__ == '__main__':
 	main()
